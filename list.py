4b3c15fb9cf30ca6404d7f18ce7c52e3a7b76168
pateldeepak <patrldeepak5498@gmail.com>
2020-06-10 23:37:50 +0530

    this is my first commit

---
+++ list.py
@@ -0,0 +1,9 @@
+print("Welcome to KBC")
+print("Start,let's play KBC")
+a=["Bharat ki rajdhani kaha hai"," Taj Mahal kaha hai","Bharat ke pahale P.M. kaun hai"]
+b=[["1.Mumbai","2.patna","3.New Delhi","4.U.P."],["1.Kanpur","2.Agra","3.Jaunpur","4.none"],["1.Rahul Gandhi","2.Dr.Rajendra Prasad","3.Narendra Modi","4.Pandit Jawahar lal Nehru"]
+# c=["3","2","4"]
+# for x in range(len(a))
+# 	print(a(i))
+# 	# print(b(i))
+	# print(c(i))
